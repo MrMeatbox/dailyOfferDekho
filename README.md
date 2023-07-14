a04a26d811e6e4f3caf9208b2bf0ebf819324a26
MrMeatbox <u1804125student.cuet.ac.bd>
2023-07-14 21:29:12 +0600

    nahid

---
+++ README.md
@@ -0,0 +1,20 @@
+# DailyOfferDekho
+
+Welcome to the DailyOfferDekho repository! This is a web application built using the LAMP stack (Linux, Apache, MySQL, PHP) that helps users find daily offers and discounts on various products.
+
+## Prerequisites
+
+Before getting started, make sure you have the following software installed on your system:
+
+- Linux
+- Apache
+- MySQL
+- PHP
+
+## Installation
+
+1. Clone the repository to your local machine:
+
+   ```bash
+   git clone https://github.com/Artics-Inc/dailyofferdekho.git
+   ```
---
+++ config.php
@@ -0,0 +1,32 @@
+<?php
+
+/*--------------------*/
+// Description: Affilink - Coupons & Deals Php Script
+// Author: Wicombit
+// Author URI: https://www.wicombit.com
+/*--------------------*/
+
+//URL PROJECT
+
+define ('SITE_URL', 'http://localhost:8888/dailyofferdekho-site');
+
+//DATABASE CONFIG
+
+$database = array(
+'host' => 'localhost',
+'db' => 'daily_offer_db',
+'user' => 'devuser',
+'pass' => 'gM8IymihkZYiYz]v'
+);
+
+$site_config = array(
+'featured_items' => 10,
+'exclusive_items' => 10,
+'featured_stores' => 10,
+'featured_locations' => 10,
+'latest_items' => 10,
+'reviews_page' => 3,
+'page_limit' => 6,
+);
+
+?>
